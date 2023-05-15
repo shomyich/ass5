9d55c42f65ed06531bf371f632ea4028239409ca
shomyich <220009@astanait.edu.kz>
2023-05-15 09:25:35 +0600

    BST class was created

---
+++ src/BST.java
@@ -0,0 +1,19 @@
+public class BST<K extends Comparable<K>, V> {
+    private Node root;
+    private class Node
+
+    {
+        private K key;
+        private V val;
+        private Node left, right;
+        public Node(K key, V val){
+        this.key = key;
+        this.val = val;
+        }
+    }
+
+
+    public void put(K key, V val) {}
+    public V get(K key) {}
+    public void delete(K key) {}
+    public Iterable<K> iterator() {}
