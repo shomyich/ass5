89cd2dee9e6ef5a13e893ab39a7f432c9e2f2d88
shomyich <220009@astanait.edu.kz>
2023-05-21 18:11:47 +0600

    The `put` method in a binary search tree is used to insert a key-value pair into the tree. It takes a key and a value as parameters. The method starts at the root of the tree and compares the key with the key of the current node.
    If the key is less than the current node's key, the method recursively moves to the left subtree. If the left subtree is empty, a new node is created with the given key-value pair and added as the left child of the current node. If the left subtree is not empty, the method continues its recursive call on the left subtree.
    
    If the key is greater than the current node's key, the method recursively moves to the right subtree. If the right subtree is empty, a new node is created with the given key-value pair and added as the right child of the current node. If the right subtree is not empty, the method continues its recursive call on the right subtree.
    
    If the key is equal to the current node's key, the method updates the value of the current node with the given value.
    
    This process continues until a suitable position is found for the new node in the tree. By recursively traversing the tree and comparing keys, the `put` method maintains the binary search tree property, where all keys in the left subtree are less than the current node's key, and all keys in the right subtree are greater.
    
    After the `put` method completes, the new key-value pair is successfully inserted into the binary search tree, preserving its ordered structure.

--- src/BST.java
+++ src/BST.java
@@ -1,19 +1,44 @@
 public class BST<K extends Comparable<K>, V> {
     private Node root;
-    private class Node
 
-    {
+    private class Node {
         private K key;
         private V val;
         private Node left, right;
-        public Node(K key, V val){
-        this.key = key;
-        this.val = val;
+
+        public Node(K key, V val) {
+            this.key = key;
+            this.val = val;
         }
     }
 
 
-    public void put(K key, V val) {}
-    public V get(K key) {}
-    public void delete(K key) {}
-    public Iterable<K> iterator() {}
+    public void put(K key, V val) {
+        root = put(root, key, val);
+    }
+
+    private Node put(Node node, K key, V val) {
+        if (node == null) {
+            return new Node(key, val);
+        }
+
+        int cmp = key.compareTo(node.key);
+        if (cmp < 0) {
+            node.left = put(node.left, key, val);
+        } else if (cmp > 0) {
+            node.right = put(node.right, key, val);
+        } else {
+            node.val = val;
+        }
+        return node;
+    }
+
+    public V get(K key) {
+    }
+
+    public void delete(K key) {
+    }
+
+    public Iterable<K> iterator() {
+    }
+    }
